781c02702ae9d9bab3536ca7f5e1739754a9009f
theYounger <kuyehara1@gmail.com>
2016-06-06 18:23:33 -1000

    js.basics test: changed 1 function

--- basics.js
+++ basics.js
@@ -1,24 +1,46 @@
 /* Create a `myName` variable and assign it a String value */
+var myName = "Kyle";
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+ var person = {
+  name: "Kyle",
+  age: 27
+ };
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+ var canDrive = function() {
+    return person.age >= 16;
+ };
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+ function greet (name){
+    console.log("Hello, my name is " + name);
+ }
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+
+ dataTypes = ["dataTest", 247, 24 > 7, {test: "value"}, undefined, [2, 4, 7, "testValue"]];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+ var dog = {
+  name: "Spot",
+  bark: function() {
+    console.log("bark");
+  }
+ };
